afc11e747634002659df0b42f085cc9544fe71a3
fatmawatiairliana-hub <fatmawatiairliana@gmail.com>
2026-07-24 14:53:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let keranjang = JSON.parse(localStorage.getItem("cart")) || [];
 
-function tambah(nama, harga){
-    keranjang.push({nama, harga});
+function simpan(){
     localStorage.setItem("cart", JSON.stringify(keranjang));
-    alert("Ditambahkan!");
+}
+
+function tambah(nama, harga){
+    let item = keranjang.find(i => i.nama === nama);
+
+    if(item){
+        item.qty += 1;
+    } else {
+        keranjang.push({nama, harga, qty:1});
+    }
+
+    simpan();
+    alert("Ditambahkan ke keranjang!");
 }
 
 function tampilKeranjang(){
@@ -11,12 +22,45 @@ function tampilKeranjang(){
     let total = 0;
     list.innerHTML = "";
 
-    keranjang.forEach(item => {
-        list.innerHTML += `<p>${item.nama} - Rp ${item.harga}</p>`;
-        total += item.harga;
+    keranjang.forEach((item, index) => {
+        let subtotal = item.harga * item.qty;
+        total += subtotal;
+
+        list.innerHTML += `
+        <div class="item">
+            <h3>${item.nama}</h3>
+            <p>Rp ${item.harga}</p>
+
+            <div class="qty">
+                <button onclick="kurang(${index})">➖</button>
+                <span>${item.qty}</span>
+                <button onclick="tambah('${item.nama}', ${item.harga})">➕</button>
+            </div>
+
+            <p>Subtotal: Rp ${subtotal}</p>
+            <button onclick="hapus(${index})">❌ Hapus</button>
+        </div>
+        `;
     });
 
     document.getElementById("total").innerText = "Total: Rp " + total;
+}
+
+function kurang(index){
+    keranjang[index].qty -= 1;
+
+    if(keranjang[index].qty <= 0){
+        keranjang.splice(index,1);
+    }
+
+    simpan();
+    tampilKeranjang();
+}
+
+function hapus(index){
+    keranjang.splice(index,1);
+    simpan();
+    tampilKeranjang();
 }
 
 function kirimWA(){
@@ -24,8 +68,8 @@ function kirimWA(){
     let total = 0;
 
     keranjang.forEach(item => {
-        teks += item.nama + " - Rp " + item.harga + "%0A";
-        total += item.harga;
+        teks += `${item.nama} (${item.qty}x) - Rp ${item.harga * item.qty}%0A`;
+        total += item.harga * item.qty;
     });
 
     teks += "Total: Rp " + total;
